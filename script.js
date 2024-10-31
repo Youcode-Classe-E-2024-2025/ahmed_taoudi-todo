7b44cd6b62a3381bc678889a5d5a9048b6c2f013
ahmed <human@localhost.localdomain>
2024-10-31 23:01:00 +0100

    aaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -15,9 +15,12 @@ const doingCount= document.getElementById('doing-count');
 const doneCount= document.getElementById('done-count');
 let  canbanCarts;
 let allTasks;
-let idc=21;
+// let idc=21;
 //  const allTasks ;
  
+welcome.addEventListener("click",()=>{
+    console.log(allTasks);
+})
 
 // addForm.reset() 
 
@@ -39,6 +42,8 @@ async function fetchTasks() {
         console.error('There was a problem with the fetch operation:', error);
     }
 }
+
+// **** affiche ****
 
 function displayTasks(allTasks) {
     let todoTasks = allTasks.filter(item => item.status==='todo' );
@@ -62,19 +67,21 @@ function displayTasks(allTasks) {
     canbanCarts = document.querySelectorAll('.canban-cart');
     addEventListenerForTask(canbanCarts)
 }
+// **** create ****
 
 function createTask(task,zone){
    
         const taskDiv = document.createElement('div');
-        taskDiv.id = task.id
+        taskDiv.id = `${task.id}`
+        taskDiv.draggable=true;
         if(task.priority=='low'){
-            taskDiv.className = 'canban-cart my-4 p-4  h-fit hover:bg-green-500 rounded-md bg-green-300';
+            taskDiv.className = 'canban-cart my-4 p-2 md:p-4 border-4 border-sky-50 h-fit hover:bg-green-500 rounded-lg bg-green-300';
 
         }else if(task.priority=='high'){
-            taskDiv.className = 'canban-cart my-4 p-4  h-fit rounded-md  hover:bg-red-500 bg-red-400';
+            taskDiv.className = 'canban-cart my-4 p-2 md:p-4 border-4 border-sky-50 h-fit rounded-lg  hover:bg-red-500 bg-red-400';
 
         }else{
-            taskDiv.className = 'canban-cart my-4 p-4  h-fit rounded-md  hover:bg-orange-500 bg-orange-300';
+            taskDiv.className = 'canban-cart my-4 p-2 md:p-4 border-4 border-sky-50 h-fit rounded-lg  hover:bg-orange-500 bg-orange-300';
 
         } 
         taskDiv.innerHTML = `
@@ -82,17 +89,17 @@ function createTask(task,zone){
                                 <!-- name -->
                                 <h3 class="text-xl"> ${task.title}</h3>
                                 <!-- edit icon -->
-                                <div class=" w-16 flex justify-between">
-                                    <div >
-                                        <svg role="button" class="edit-icon h-5  fill-slate-800" xmlns="http://www.w3.org/2000/svg"
+                                <div class=" w-10 flex flex-col items-end justify-between">
+                                    <div class="hover:scale-110">
+                                        <svg role="button" class="edit-icon h-5  fill-slate-800 hover:fill-slate-100  " xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                             <path
                                                 d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z" />
                                         </svg>
                                     </div>
                                     <!-- delete icon -->
-                                    <div >
-                                        <svg role="button" class="delete-icon h-5 fill-slate-800" xmlns="http://www.w3.org/2000/svg"
+                                    <div class="hover:scale-110  mt-8">
+                                        <svg role="button" class="delete-icon h-5 fill-slate-800 hover:fill-slate-100 " xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                             <path
                                                 d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z" />
@@ -106,9 +113,12 @@ function createTask(task,zone){
            
         `;
         zone.appendChild(taskDiv);
+        // zone.insertBefore(taskDiv, zone.firstChild);
+        
         // canbanCarts = document.querySelectorAll('.canban-cart');
  
 }
+// **** delete ****
 
 function deleteTask(taskId){
     // need work
@@ -117,11 +127,12 @@ function deleteTask(taskId){
     console.log(index,"index");
     if (index !== -1) {
         allTasks.splice(index, 1);
-    }
+    } 
     // allTasks =allTasks.filter(tsk=> tsk.id !== taskId);
     displayTasks(allTasks); 
 }
 // **** edit ****
+
 function editTask(taskId){
     // need work
     mainPage.classList.add('blur');
@@ -140,7 +151,7 @@ return index;
 // allTasks[index]=newValue;
 
 }
-
+ 
 function displayDetails(selectedTask ){
     
     
@@ -193,6 +204,14 @@ const newTaskBtn = document.querySelector('#new-task-btn');
 const addTaskBtn = document.querySelector('#add-task-btn');
 // edit
 const editTaskBtn = document.querySelector('#edit-task-btn');
+const sortTasksBtn = document.querySelector('#sort-tasks-btn');
+
+sortTasksBtn.addEventListener('click',()=>{
+    const sortedTasks = allTasks.sort((tsk1,tsk2)=>{
+      return  tsk1.dueDate < tsk2.dueDate ? -1:1
+    })
+    displayTasks(sortedTasks);
+})
 
 // Cancel add
 const cancelAddTaskBtn = document.querySelector('#cancel-add-task-btn');
@@ -204,10 +223,10 @@ const cancelEditTaskBtn = document.querySelector('#cancel-edit-task-btn');
 const searchIcon = document.querySelector('#search-icon');
 
 // add task listener
-addTaskBtn.addEventListener('click', () => {
-    
+addTaskBtn.addEventListener('click', (ev) => {
+   
         const newTask={
-            id: idc++,
+            id: Date.now, //id simple by time 
             title: addForm['taskName'].value ,
             description: addForm['desc'].value ,
             status: addForm['taskStatus'].value ,
@@ -243,7 +262,7 @@ cancelAddTaskBtn.addEventListener('click', () => {
 
 cancelEditTaskBtn.addEventListener('click', () => {
     // mainPage
-    editForm.reset();
+    // editForm.reset();
     mainPage.classList.remove('blur');
     formEditZone.classList.add('hidden');
 })
@@ -276,7 +295,7 @@ function addEventListenerForTask(canbanCarts){
                 // console.log('All Tasks:', allTasks); 
                 // console.log(allTasks);
                displayTasks(allTasks);
-               editForm.reset();
+            //    editForm.reset();
                mainPage.classList.remove('blur');
                formEditZone.classList.add('hidden');
             //    editForm.reset()  
@@ -315,26 +334,3 @@ taskDetails.addEventListener('click', ( event) => {
     mainPage.classList.remove('blur');
     taskDetails.classList.add('hidden');
 })
-
-function validateFormInputs(){
-    // console.log(addForm['taskName'].value); 
-    // console.log(addForm['deadLine'].value); 
-    // console.log(addForm['desc'].value); 
-    // console.log(addForm['taskLevel'].value,"low"); 
-    // console.log(addForm['taskStatus'].value,"do"); 
-const newTask={
-            id: 22,
-            title: addForm['taskName'].value ,
-            description: addForm['desc'].value ,
-            status: addForm['taskStatus'].value ,
-            dueDate: addForm['deadLine'].value,
-            priority: addForm['taskLevel'].value 
-        }; 
-        // console.log(newTask);
-        allTasks.push(newTask)
-        console.log(allTasks);
-       displayTasks(allTasks);
-
-    //    createTask(newTask,_doing)
-    //    displayTasks(allTasks);
-};
